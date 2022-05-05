f293649220a3c05798579cd8036f22ecb839b0e1
Vinicius Rodrigues <vinicius.rodrigues.lima@ccc.ufcg.edu.br>
2022-05-05 18:07:14 -0300

    Add quicksort explanation

--- src/main/java/com/looqbox/vinicius/pokeapi/sorting/QuickSort.java
+++ src/main/java/com/looqbox/vinicius/pokeapi/sorting/QuickSort.java
@@ -3,6 +3,30 @@ package com.looqbox.vinicius.pokeapi.sorting;
 import java.util.Comparator;
 import java.util.List;
 
+/* 
+    O quicksort é um algoritmo de complexidade O(n log n) 
+    que funciona utilizando recursão.
+
+    No geral, ele funciona da seguinte forma: os elementos são particionados
+    em duas partes e um pivot, na qual a parte da direita tem os elementos maiores
+    que o pivot e a parte esquerda tem os elementos menores que o pivot. No processo
+    de particionamento o pivot fica na posição correta dele, ou seja, ele não será 
+    mais alterado até o final da ordenação. Em seguida, esse processo é
+    feito com as outras duas partes separadamente.
+    Isso é feito até que se chegue em uma situação trivial em que temos
+    apenas um ou nenhum elemento na parte.
+
+    No particionamento a primeira coisa a ser feita é escolher o pivot (nesse caso, 
+    foi escolhido o último elemento por facilidade mas qualquer outro elemento serviria).
+    Utilizo um índice para marcar o final da parte da esquerda (leftIndex) e vou iterando
+    pelos elementos e testando se são maiores ou menores que o pivot, se são menores
+    eu troco a posição dele com o elemento da posição leftIndex (utilizando o swap) e
+    incremento o leftIndex. Ao final da iteração eu chego na posição anterior à posição 
+    do pivot, ou seja, ele ainda está na última posição. Assim, no último comando eu troco
+    ele de lugar com o elemento da posição leftIndex que é o primeiro elemento da parte 
+    direita. Dessa forma o pivot fica dividindo as partes. O que estiver do lado esquerdo
+    dele é menor que ele e o que estiver do lado direito é maior.
+*/
 public class QuickSort<T> implements Sort<T> {
 
     @Override
